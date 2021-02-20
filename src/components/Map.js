2e6dad40a0ee335db8b96ea48a42b0c698873e7c
samsonc <samsonc0514@gmail.com>
2021-02-20 14:32:18 -0500

    Updated

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow, } from '@react-google-maps/api';
-import { formatRelative } from "date-fns";
 import MapStyle from './MapStyle';
 import index from '../index.css';
 
