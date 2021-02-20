e14751a3029264b196cd2a40cef3d2184356a4ad
samsonc <samsonc0514@gmail.com>
2021-02-20 07:55:20 -0500

    Fixed Map import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Map from './Map';
+import Map from './components/Map';
 
 function App() {
   return (
