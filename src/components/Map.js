97a128350d2be71519c714bd9dbb5f950fe624c9
samsonc <samsonc0514@gmail.com>
2021-02-20 09:02:55 -0500

    Fixed Google Map API Key Issue

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,42 +1,58 @@
 import React from 'react';
-import { GoogleMap, useLoadScript, Marker, InfoWindow, } from "@react-google-maps/api";
-// import { formatRelative } from "date-fns";
-import MapStyle from "./MapStyle"
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow,  } from '@react-google-maps/api';
+import { formatRelative } from "date-fns";
+import MapStyle from './MapStyle'
 
 const libraries = ["places"];
 
-const mapContainerStyle = {
+const containerStyle = {
     width: "50vw",
-    height: "50vw",
+    height: "51vw",
 };
+
 const center = {
     lat: 43.6532, 
     lng: -79.3832,
 };
+
 const options = {
     styles: MapStyle, 
 };
 
 function Map() {
-    const { isLoaded, loadErr } = useLoadScript({
-        // Key: 'AIzaSyAD5HWvSSyyhaTKklV9V2beb5xgjRJEgLQ',
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, 
-        libraries,
-    });
-    if (loadErr) return "Err loading maps";
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyAD5HWvSSyyhaTKklV9V2beb5xgjRJEgLQ", 
+    libraries,
+  });
+
+    if (loadError) return "Err loading maps";
     if (!isLoaded) return "Loading Maps";
-    return (
-        <div>
-            <GoogleMap 
-            mapContainerStyle={mapContainerStyle}
-            zoom={8}
-            center={center}
-            options={options}
-            ></GoogleMap>
-            
-        </div>
 
+//   const [map, setMap] = React.useState(null)
 
-    )
+//   const onLoad = React.useCallback(function callback(map) {
+//     const bounds = new window.google.maps.LatLngBounds();
+//     map.fitBounds(bounds);
+//     setMap(map)
+//   }, [])
+
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        // onLoad={onLoad}
+        // onUnmount={onUnmount}
+      >
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
+
 export default Map;
