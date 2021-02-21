27cbcb6ae33dae438eb0ab9ceedf110818b6e4b0
samsonc <samsonc0514@gmail.com>
2021-02-20 23:04:35 -0500

    Fixed searching issue

---
+++ src/components/Home.js
@@ -0,0 +1,128 @@
+import Navigation from "./Navigagion";
+import Footer from "./Footer";
+import { Button, Container, Jumbotron, Row, Card, Form, Col } from "react-bootstrap";
+import { faSearchLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from "react";
+
+function Home() {
+
+  const [formHousing, setFormHousing] = useState("0");
+
+  return (
+    <div class="Home">
+      <Navigation/>
+
+      <Container fluid style={{margin: "0", backgroundImage: "url(greencity.jpg)", backgroundPosition: "center", paddingTop: "10%", paddingBottom: "10%"}}>
+        <Row className="justify-content-md-center">
+          <h1 className="display-4 text-white">Find your nearest Sharger</h1><br/>
+          <br/>
+        </Row>
+        <Row className="justify-content-md-center">
+          <p className="lead text-white">Join thousands of EV owners in North America.</p>
+          <br/>
+        </Row>
+        <br/><br/>
+        <Row className="justify-content-md-center">
+          <Card style={{width:"30em", padding: "3% 2%"}}>
+            <Card.Title className="text-center">Find a Sharger</Card.Title>
+            <Card.Text className="text-muted text-center">Just takes a few simple steps</Card.Text>
+            <hr/>
+            <Form>
+              <Form.Group controlId="formLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Control type="text" placeholder="Enter your location" />
+              </Form.Group>
+
+              <Row>
+                <Col>
+                  <Form.Group controlId="formChargerType">
+                    <Form.Label>Plug Type</Form.Label>
+                    <Form.Control as="select" custom>
+                      <option>type a</option>
+                      <option>type b</option>
+                      <option>type c</option>
+                      <option>type d</option>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="formChargerWattage">
+                    <Form.Label>Charger Wattage</Form.Label>
+                    <Form.Control as="select" custom>
+                      <option>wattage range a</option>
+                      <option>wattage range b</option>
+                      <option>wattage range c</option>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+
+              </Row>
+              
+
+              <Form.Group controlId="formHousing">
+                <Form.Label>With Housing?</Form.Label>
+                <div key={`inline-radio`} className="mb-3">
+                  <Form.Check inline value="1" label="Yes" type="radio" onChange={e => setFormHousing(e.target.value)} checked={formHousing==="1"}/>
+                  <Form.Check inline value="0" label="No" type="radio" onChange={e => setFormHousing(e.target.value)} checked={formHousing==="0"}/>
+                </div>
+              </Form.Group>
+              <br/>
+              <br/>
+              <Form.Group>
+                <Button variant="light" type="submit" size="lg" block><FontAwesomeIcon icon={faSearchLocation}/> Find a Sharger</Button>
+              </Form.Group>
+            </Form>
+          </Card>
+        </Row>
+      </Container>
+
+      <Jumbotron style={{margin: "0"}} fluid>
+        <Container>
+          <br/><br/><br/><br/>
+          <h1 className="display-4 text-right">Have a vacant EV charging spot?</h1><br/>
+          <p className="lead text-right">Become a Sharger host and make some money off your spare charger.</p>
+          <br/><br/>
+          <Button href="#" variant="outline-primary" className="float-right" size="lg">
+            <FontAwesomeIcon icon={faSearchLocation} /> Host a Sharger
+          </Button>
+          <br/><br/>
+          <br/><br/><br/><br/>
+        </Container>
+      </Jumbotron>
+
+      <Jumbotron style={{margin: "0"}} fluid style={{backgroundColor: "white"}}>
+        <Container>
+          <br/><br/><br/><br/>
+          <h1 class="display-4">Make the switch</h1><br/>
+          <p class="lead">Since 2024, Sharger has inderectly deducted 2% of global carborn emission, by making EV ownership a better experience</p>
+          <br/><br/>
+          <Button href="#" variant="outline-primary" size="lg">
+            Learn More
+          </Button>
+          <br/><br/><br/><br/>
+        </Container>
+      </Jumbotron>
+
+
+      <Jumbotron style={{margin: "0"}} fluid>
+        <Container>
+          <br/><br/><br/><br/>
+          <h1 className="display-4 text-right">Thinking about collaborating with us?</h1><br/>
+          <p className="lead text-right">Become a Sharger Partner and add your company to the Sharger network.</p>
+          <br/><br/>
+          <Button href="#" variant="outline-primary" className="float-right" size="lg">
+            Contact
+          </Button>
+          <br/><br/>
+          <br/><br/><br/><br/>
+        </Container>
+      </Jumbotron>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
+
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,174 +1,195 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader, Marker, InfoWindow, } from '@react-google-maps/api';
-import MapStyle from './MapStyle';
-import index from '../index.css';
-
+import React from "react";
+import { GoogleMap, useLoadScript, Marker, InfoWindow, } from "@react-google-maps/api";
 import usePlacesAutocomplete, { getGeocode, getLatLng, } from "use-places-autocomplete";
 import {
-    Combobox,
-    ComboboxInput,
-    ComboboxPopover,
-    ComboboxList,
-    ComboboxOption, } from "@reach/combobox";
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 
-import "@reach/combobox/styles.css"; 
+import "@reach/combobox/styles.css";
+import MapStyle from "./MapStyle";
+import Navigation from './Navigagion';
+import Footer from './Footer';
+import index from '../index.css';
+import ListingCard from './ListingCard';
+
+
 const libraries = ["places"];
-
-const containerStyle = {
-    width: "100%",
-    height: "100vh",
+const mapContainerStyle = {
+  width: "100%",
+  height: "95vh",
 };
-
-const center = {
-    lat: 43.6532,
-    lng: -79.3832,
-};
-
 const options = {
-    styles: MapStyle,
-    disableDefaultUI: true,
-    zoomControl: true,
+  styles: MapStyle,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
 };
 
 function Map() {
-    const { isLoaded, loadError } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyAD5HWvSSyyhaTKklV9V2beb5xgjRJEgLQ",
-        libraries,
-    });
-    const [markers, setMarkers] = React.useState([])
-    const [selecters, setSelecters] = React.useState(null)
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAD5HWvSSyyhaTKklV9V2beb5xgjRJEgLQ",
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
 
-    const onMapClick = React.useCallback((e) => {
-        setMarkers((current) => [...current,
-        {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            time: new Date(),
-        },
-        ]);
-    }, []);
-    // without using renders
-    const mapRef = React.useRef();
-    const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-    }, []);
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+      },
+    ]);
+  }, []);
 
-    const panningTo = React.useCallback(({ lat, lng }) => {
-        mapRef.current.panningTo({ lat, lng });
-        mapRef.current.setZoom(18);
-    }, []);
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
-    if (loadError) return "Err loading maps";
-    if (!isLoaded) return "Loading Maps";
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(18);
+  }, []);
 
-    return (
-        <div>
-            {/* <div>
-                <h1>Sharger</h1>
-            </div> */}
-            <Search panningTo={panningTo} />
-            <GoogleMap
-                id="map"
-                mapContainerStyle={containerStyle}
-                zoom={12}
-                center={center}
-                options={options}
-                onClick={onMapClick}
-                onLoad={onMapLoad}
-            // onUnmount={onUnmount}
-            >
-                {markers.map((marker) => (
-                    <Marker
-                        key={marker.time.toISOString()}
-                        position={{ lat: marker.lat, lng: marker.lng }}
-                        icon={{
-                            url: "/sharger-marker.jpg",
-                            // Marker Size
-                            scaledSize: new window.google.maps.Size(40, 40),
-                            // Changing location of point of click
-                            origin: new window.google.maps.Point(0, 0),
-                            anchor: new window.google.maps.Point(20, 30),
-                        }}
-                        // show info box whenever the marker is clicked
-                        onClick={() => {
-                            setSelecters(marker);
-                        }}
-                    />
-                ))}
-                {selecters ? (
-                    <InfoWindow
-                        position={{ lat: selecters.lat, lng: selecters.lng }}
-                        onCloseClick={() => {
-                            // close info box whenever the marker is clicked
-                            setSelecters(null);
-                        }}>
-                        <div>
-                            <h2>Review</h2>
-                        </div>
-                    </InfoWindow>
-                ) : null}
-            </GoogleMap>
-        </div>
-    );
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
+  return (
+    <div>
+       <Navigation/>
+       <div className="Map">
+
+      {/* <Locate panTo={panTo} /> */}
+      <Search panTo={panTo} />
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: `/sharger-marker.jpg`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(30, 20),
+              scaledSize: new window.google.maps.Size(60, 60),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                Sharger Name
+              </h2>
+              <p>User Name</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-function Search({ panningTo }) {
-    const { ready, value, suggestions: { status, data }, setValue, clearSuggestions } = usePlacesAutocomplete({
-        requestOptions: {
-            location: {
-                lat: () => 43.6532,
-                lng: () => -79.3832
-            },
-            radius: 200 * 1000,
-        },
-    });
+// function Locate({ panTo }) {
+//   return (
+//     <button
+//       className="locate"
+//       onClick={() => {
+//         navigator.geolocation.getCurrentPosition(
+//           (position) => {
+//             panTo({
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude,
+//             });
+//           },
+//           () => null
+//         );
+//       }}
+//     >
+//       <img src="/compass.svg" alt="compass" />
+//     </button>
+//   );
+// }
 
-    const handleInput = (e) => {
-        setValue(e.target.value);
-      };
-    
-    const handleSelect = async (address) => {
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 43.6532, lng: () => -79.3832 },
+      radius: 200 * 1000,
+    },
+  });
+
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
     setValue(address, false);
     clearSuggestions();
 
     try {
-        const result = await getGeocode({ address });
-        const { lat, lng } = await getLatLng(result[0]);
-        panningTo({ lat, lng });
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
     } catch (error) {
-        console.log("Error: ", error);
+      console.log("Error: ", error);
     }
-    };
-    
-    return (
-        <div className="SearchEngine">
-            <Combobox 
-            onSelect={(address) => {
-                console.log(address);
-            }}
-            >
-                {/*  onSelect={handleSelect} */}
-                <ComboboxInput 
-                    value={value} 
-                    onChange={handleInput} 
-                    disabled={!ready}
-                    placeholder="Find a Sharger"
-                />
-                <ComboboxPopover>
+  };
 
-                    <ComboboxList>
-                        {status === "OK" && data.map(({ id, description }) => ( 
-                            <ComboboxOption key={id} value={description} />
-                        ))}
-                    </ComboboxList>
-                    
-                </ComboboxPopover>
-            </Combobox>
-        </div>
-
-    );
-
+  return (
+    <div className="SearchEngine">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Finder A Sharger"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
 }
-
 export default Map;
